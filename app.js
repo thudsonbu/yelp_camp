30d5b16b37a76394ee9ac7af2cb7bd0879871933
thudsonbu <hudson_thom@bentley.edu>
2020-06-15 01:45:08 +0000

    Fixed expired image urls

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var campgrounds = [
-		{name: "Red Rocks", image: "https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf8525478487d2a79d2964a_340.jpg"},
-		{name: "Jackson Valley", image: "https://pixabay.com/get/57e1d14a4e52ae14f1dc84609620367d1c3ed9e04e50744074277ed6914cc2_340.jpg"},
-		{name: "North Eagle", image: "https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e50744074277ed6914cc2_340.jpg"},];
+		{name: "Red Rocks", image: "https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e507440772e7add934ac5_340.jpg"},
+		{name: "Jackson Valley", image: "https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e507440772e7add934ac5_340.jpg"},
+		{name: "North Eagle", image: "https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507440772e7add934ac5_340.jpg"},];
 
 
 // Memorize this line
@@ -24,7 +24,7 @@ app.get("/", function(req, res){
 app.get("/campgrounds", function(req, res) {
 	
 	// the first campgrounds is the name we give the variable, the second is the data
-	res.render("campgrounds", {campgrounds:campgrounds});
+	res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 // campgrounds post route
